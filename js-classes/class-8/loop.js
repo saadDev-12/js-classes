19d621c732660c2c25d93a381ebb00196029acc1
saadDev-12 <saad90dev@gmail.com>
2026-09-12 22:56:10 -0700

    loop

---
+++ js-classes/class-8/loop.js
@@ -0,0 +1,25 @@
+// while loop 
+// let i = 1;
+// while (i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// let password = false;
+// let attempt=3;
+// let i = 1;
+// while( !password && i <= attempt){
+//     console.error("please enter correct correct");
+//     i++
+// }
+
+// Do-while loop
+
+let i  = 1;
+do{
+    console.log("show menu");
+    i++
+}
+while(i <= 5);
